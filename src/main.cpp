ebdb5e0eca2c4807ffde9a86bd985cdf92a06c32
Peter Lilley <71025767+PeteLilley@users.noreply.github.com>
2022-10-31 18:42:39 -0400

    Created simple network tables test program

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,19 @@
+#include <networktables/NetworkTable.h>
+#include <networktables/NetworkTableInstance.h>
+#include <unistd.h>
+
 int main(int argc, char** argv) {
+  nt::NetworkTableInstance nt_inst = nt::NetworkTableInstance::GetDefault();
+
+  std::shared_ptr<nt::NetworkTable> sd_table = nt_inst.GetTable("SmartDashboard"),
+                                    fms_table = nt_inst.GetTable("FMSInfo");
+
+  nt_inst.StartClientTeam(1511);
+
+  while (true) {
+    sd_table->PutNumber("Raspberry Pi is working :D", 1234.5);
+    sleep(1);
+  }
+
   return 0;
 }
