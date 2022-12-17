3c908002a28d155729486b0763208d83e586b519
Peter Lilley <71025767+PeteLilley@users.noreply.github.com>
2022-12-16 21:31:12 -0500

    Updated config header path in gitignore

--- src/include/RollingRaspberry/config/config.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <RollingRaspberry/rolling_raspberry.h>
-
-#define THUNDER_CONFIG_DIR          std::filesystem::path("/Users/peterplilleyiii/dev/Robotics/RollingRaspberry/config")
-#define THUNDER_VISION_CONFIG_JSON  (THUNDER_CONFIG_DIR / "vision.json")
-#define THUNDER_FIELD_CONFIG_JSON   (THUNDER_CONFIG_DIR / "field.json")
