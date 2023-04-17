4ed70a61b86818b3983039c97576432212b2b9ca
Peter Lilley <71025767+PeteLilley@users.noreply.github.com>
2023-04-16 20:28:01 -0400

    Updated MountUSBDrive systemd service to always restart and continuously handle plugged/unplugged drives

--- scripts/setup_drive.sh
+++ scripts/setup_drive.sh
@@ -6,18 +6,28 @@ if [ $EUID -ne 0 ]; then
   exit 1
 fi
 
+DEV_MOUNTED=false
+
 if [ "$(ls -A /media/usb)" ]; then
   echo 'Device already mounted at /media/usb/'
-  exit 0
+  DEV_MOUNTED=true
 fi
 
-
 if [ "$(lsblk | grep 'sda1')" ]; then
-  sudo mount /dev/sda1 /media/usb
-  echo 'Mounted /dev/sda1 at /media/usb'
+  if [ "$DEV_MOUNTED" = false ]; then
+    sudo mount /dev/sda1 /media/usb
+    echo 'Mounted /dev/sda1 at /media/usb'
+  fi
+
   exit 0
 else
   echo 'Nothing at /dev/sda1 to mount'
+
+  if [ "$DEV_MOUNTED" = true ]; then
+    sudo umount /media/usb
+    echo 'Unmounted device at /media/usb'
+  fi
+
   exit 1
 fi
 
